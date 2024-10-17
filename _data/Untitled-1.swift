d3aed6d0ee70503e11e3c36a55128bc860aa3752
jagar2 <jca92@drexel.edu>
2024-10-17 20:36:09 +0000

    update with presentations need to add more modern ones

---
+++ _data/Untitled-1.swift
@@ -0,0 +1,207 @@
+
+- talk_title: "invited Presentations"
+  type: invited_presentations
+    - talk_title: "Practical Deployment of Machine Learning for High-Velocity Science"
+      date: "September 11, 2023"
+      location: "Busan, Korea"
+      conference: "International Microscopy Conference"
+
+    - talk_title: "Real-Time Machine Learning in 4D-STEM"
+      date: "August 22, 2023"
+      location: "KAIST"
+      conference: "M3I3 Workshop"
+
+    - talk_title: "Practical and Parsimonious Real-Time Analysis in Materials Microscopy"
+      date: "August 21, 2023"
+      location: ""
+      conference: "Microscopy and Microanalysis 2023"
+
+    - talk_title: "Real-Time Physics-Constrained Machine Learning in Multidimensional Atomic Force Microscopy"
+      date: "June 20, 2023"
+      location: ""
+      conference: "SPM Connect Conference 2023"
+
+    - talk_title: "Practical Deployment of Machine Learning for High Velocity Materials Science"
+      date: "May 26, 2023"
+      location: "University of Michigan"
+      conference: "Special Seminar"
+
+    - talk_title: "Practical Deployment of Machine Learning for High Velocity Materials Science"
+      date: "November 9, 2023"
+      location: "NUANCE Northwestern"
+      conference: ""
+
+    - talk_title: "Codesign of Parsimonious Machine Learning for High Velocity Materials Microscopy on the Edge"
+      date: "October 6, 2022"
+      location: ""
+      conference: "National Academies of Engineering, Practical Deployment of Machine Learning Materials in Extreme Environments"
+
+    - talk_title: "New Monitoring Tools and Data-Driven Approaches for High Velocity Science"
+      date: "September 16, 2022"
+      location: "KAIST"
+      conference: "M3I3 Workshop"
+
+    - talk_title: "Data-Driven Design, Manufacturing, Understanding, and Control of Responsive and Agile Multifunctional Materials"
+      date: "February 15, 2022"
+      location: "Case Western Reserve University"
+      conference: "Department of Materials Science and Engineering Seminar"
+
+    - talk_title: "Data-Driven Design, Manufacturing, Understanding, and Control of Responsive and Agile Multifunctional Thin Films"
+      date: "January 31, 2022"
+      location: "Drexel University"
+      conference: "Mechanical Engineering and Mechanics Department Seminar"
+
+    - talk_title: "Real-Time Physics-Constrained Machine Learning in Multimodal Scanning Probe Microscopy"
+      date: "November 29, 2021"
+      location: "Takamatsu, Japan (Virtual)"
+      conference: "The 9th International Symposium on Surface Science"
+
+    - talk_title: "‘Intelligent’ and Practical Machine Learning in Materials Science"
+      date: "September 24, 2021"
+      location: "University of Texas Arlington"
+      conference: "Materials Science Seminar"
+
+    - talk_title: "Challenges and Opportunities in Data Enabled Materials Science and Engineering"
+      date: "June 15, 2021"
+      location: "Norwegian University of Science and Technology"
+      conference: "Electron Microscopy Seminar"
+
+    - talk_title: "Real-Time Machine Learning in Scanning Probe Microscopy"
+      date: "April 15, 2021"
+      location: ""
+      conference: "ISAF-PFM"
+
+    - talk_title: "Machine Learning on Hardware Accelerators for Materials Discovery"
+      date: "August 24, 2021"
+      location: ""
+      conference: "M3I3 Workshop"
+
+    - talk_title: "Challenges and Opportunities in Data Enabled Materials Science and Engineering"
+      date: "February 22, 2021"
+      location: "University of Pittsburgh"
+      conference: "Department of Chemical and Petroleum Engineering Seminar"
+
+    - talk_title: "Real-Time Machine Learning for Materials Imaging"
+      date: "November 30, 2020"
+      location: ""
+      conference: "Fast Machine Learning for Science"
+
+    - talk_title: "Real-Time Machine Learning in Materials Spectroscopy"
+      date: "November 16, 2020"
+      location: ""
+      conference: "Oxford Instruments Working Group"
+
+    - talk_title: "Real-Time Machine Learning in Materials Science and Engineering"
+      date: "August 20, 2020"
+      location: ""
+      conference: "FastML Working Group"
+
+    - talk_title: "Detecting Wallpaper Group Symmetry using Deep Convolutional Neural Networks"
+      date: "December 14, 2019"
+      location: ""
+      conference: "Workshop on Atomic Force Microscopy for Advanced Functional Materials (AFM 2 2019)"
+
+    - talk_title: "Lehigh Center for Nano-Human Interfaces"
+      date: "December 12, 2019"
+      location: ""
+      conference: "The First Great Bay Area Workshop on Emerging Oxide Electronics for Information and Health"
+
+    - talk_title: "Rapid Characterization of Functional Materials using ‘Artificial Intelligence’"
+      date: "December 10, 2019"
+      location: "Sungkyunkwan University"
+      conference: "Department of Materials Science Seminar"
+
+    - talk_title: "Rapid Characterization of Functional Materials using ‘Artificial Intelligence’"
+      date: "December 9, 2019"
+      location: "Korean Advanced Institute of Science and Technology"
+      conference: "Materials Science Seminar"
+
+    - talk_title: "Automatic Feature Extraction from Hyperspectral Imagery using Deep Recurrent Neural Networks"
+      date: "September 23, 2019"
+      location: ""
+      conference: "Workshop I: From Passive to Active: Generative and Reinforcement Learning with Physics"
+
+    - talk_title: "Automatic Feature Extraction from Hyperspectral Imagery using Deep Recurrent Neural Networks"
+      date: "September 5, 2019"
+      location: ""
+      conference: "Frontiers of Electron Microscopy and Materials Science"
+
+    - talk_title: "Deducing Inference from Hyperspectral Imaging of Materials via Machine Learning"
+      date: "August 15, 2019"
+      location: ""
+      conference: "Techbridge PA"
+
+    - talk_title: "Data-Driven Approaches for Experimental Science"
+      date: "August 12, 2019"
+      location: ""
+      conference: "Center for Nanophase Materials Sciences User Meeting"
+
+    - talk_title: "Deducing Inference from Hyperspectral Imaging of Materials Using Recurrent Neural Networks"
+      date: "May 29, 2019"
+      location: ""
+      conference: "Physics in Machine Learning Workshop"
+
+    - talk_title: "Deducing Inference from Hyperspectral Imaging of Materials Using Recurrent Neural Networks"
+      date: "May 23, 2019"
+      location: ""
+      conference: "Foundational and Applied Data Science for Molecular and Material Science & Engineering Conference"
+
+    - talk_title: "Nanoscale Susceptibilities in Ferroelectric Thin Films: Insights from Multidimensional Spectroscopy and Machine Learning"
+      date: "April 18, 2019"
+      location: "Morgan State University"
+      conference: "Department of Physics Seminar"
+
+    - talk_title: "Nanoscale Susceptibilities in Ferroelectric Thin Films: Insights from Multidimensional Spectroscopy and Machine Learning"
+      date: "December 18, 2018"
+      location: "Harbin Institute of Technology, Shenzhen"
+      conference: "Department of Materials Science and Engineering Seminar"
+
+    - talk_title: "Nanoscale Susceptibilities in Ferroelectric Thin Films: Insights from Multidimensional Spectroscopy and Machine Learning"
+      date: "December 15, 2018"
+      location: ""
+      conference: "2018 Workshop on Atomic Force Microscopy for Advanced Functional Materials"
+
+    - talk_title: "Nanoscale Susceptibilities in Ferroelectric Thin Films: Insights from Multidimensional Spectroscopy and Machine Learning"
+      date: "December 10, 2018"
+      location: "Fudan University"
+      conference: "Department of Materials Science and Engineering Seminar"
+
+    - talk_title: "Nanoscale Susceptibilities in Ferroelectric Thin Films: Insights from Multidimensional Spectroscopy and Machine Learning"
+      date: "November 7, 2018"
+      location: "Drexel University"
+      conference: "Department of Materials Science and Engineering Seminar"
+
+    - talk_title: "Nanoscale Susceptibilities in Ferroelectric Thin Films: Insights from Multidimensional Spectroscopy and Machine Learning"
+      date: "June 28, 2018"
+      location: ""
+      conference: "CIMTEC 2018 - 14th International Ceramics Congress & 8th Forum on New Materials"
+
+    - talk_title: "Extracting Physical Insight from Multidimensional Scanning Probe Spectroscopy Using Artificial Intelligence"
+      date: "June 18, 2018"
+      location: ""
+      conference: "ISAF-FMA-AMF-AMEC-PFM Joint Conference"
+
+    - talk_title: "Nanoscale Susceptibilities in Ferroelectric Thin Films: Insights from Multidimensional Spectroscopy and Machine Learning"
+      date: "February 2, 2018"
+      location: "University of Connecticut"
+      conference: "Department of Materials Science and Engineering Seminar"
+
+    - talk_title: "Nanoscale Susceptibilities in Ferroelectric Thin Films: Insights from Multidimensional Spectroscopy and Machine Learning"
+      date: "January 8, 2018"
+      location: "Virginia Commonwealth University"
+      conference: "Department of Mechanical Engineering"
+
+    - talk_title: "Controlling Ferroic Order–Combining Synthesis, Multidimensional Spectroscopy, and Data Science"
+      date: "August 10, 2017"
+      location: "Georgia Institute of Technology"
+      conference: "Department of Materials Science and Engineering Seminar"
+
+    - talk_title: "Controlling Ferroic Order–Combining Synthesis, Multidimensional Spectroscopy, and Data Science"
+      date: "March 8, 2017"
+      location: "University of Buffalo"
+      conference: "Department of Materials Design and Innovation Seminar"
+
+    - talk_title: "Controlling Ferroic Order–Combining Synthesis, Multidimensional Spectroscopy, and Data Science"
+      date: "February 8, 2017"
+      location: "Boston University"
+      conference: "Department of Mechanical Engineering Seminar"
